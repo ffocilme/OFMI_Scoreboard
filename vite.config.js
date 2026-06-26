1a439732983b90413497d1eb9a7910ac93e74ecf
Felipe Focil <82300868+ffocilme@users.noreply.github.com>
2026-06-25 22:50:51 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
  
 export default defineConfig({
   plugins: [react()],
-  base: '/OFMIScoreboard/',
+  base: '/OFMI_Scoreboard/',
   server: {
     proxy: {
       // Proxies the full scoreboard HTML page — the same URL that worked before,
